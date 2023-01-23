349dcb5c018d810f9e0a7b0575e347d891217a41
Igor-S-M <igorsousa3148@gmail.com>
2023-01-23 05:05:11 -0300

    fix: plate schema

--- src/schemas/plates-schema.ts
+++ src/schemas/plates-schema.ts
@@ -2,11 +2,11 @@ import Joi from "joi";
 import { Plate } from "../protocols/Plate.js";
 
 const plateSchema = Joi.object<Plate>({
-  name: Joi.string().required(),
-  price: Joi.number().min(0),
+  name: Joi.string().required().min(3),
+  price: Joi.number().integer().min(0),
   description: Joi.string().required(),
-  cookingTime: Joi.number().min(0),
-  type: Joi.string().required(),
+  cookingTime: Joi.number().integer().min(10),
+  type: Joi.string().required().valid("entrada","sobremesa","prato principal"),
 });
 
 export { plateSchema };
